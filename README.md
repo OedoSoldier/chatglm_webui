040ad8916fba87a88933a8f6b1cffc9cb09ee9bd
HuGuang@WSL <1395943920@qq.com>
2023-03-18 14:45:43 +0800

    feat: add promote

--- README.md
+++ README.md
@@ -15,7 +15,18 @@ Gradio 版本必须大于 3.21.0！
 ### 实参
 
  - `--path`：指定模型所在文件夹
+ - `--tokenize_path` / `--t_path`：指定模型的 `tokenize` 所在文件夹(方便加载微调后的模型)
  - `--low_vram`：4-bit 量化，6GB 显存可用
  - `--med_vram`：8-bit 量化，10GB 显存可用
  - `--cpu`：CPU运行，32G 内存可用
  - `--low_ram`：CPU运行，16G 内存可用
+ - `--example`: 示例文件的文件名
+ - `--log`: 保存对话记录的目录
+
+## Promotes 和示例
+
+提供可以在输入时使用的 example ，在 `example.txt` 中，每行一句，启动时加载。
+
+提供在对话开头使用的 `Promotes` ,在 `promotes` 文件夹中,每个文件包含一组，可以点击 `加载promotes` 刷新。
+
+`Promotes` 格式与对话记录格式一致，如果 `output` 为空数组，则由模型生成第一句回复。如果`output` 不为空，则将 `Promotes` 和对应的 `output` 注入历史（可以迫使模型接受某些设定）
--- main.py
+++ main.py
@@ -3,21 +3,42 @@ import os
 import json
 import datetime
 import gradio as gr
+import torch
 from transformers import AutoTokenizer, AutoModel
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='ChatGLM Arguments')
+    parser = argparse.ArgumentParser(description="ChatGLM Arguments")
 
-    parser.add_argument('--path', default='chatglm-6b', help='The path of ChatGLM model')
+    parser.add_argument(
+        "--path", default="chatglm-6b", help="The path of ChatGLM model"
+    )
+    parser.add_argument(
+        "--example", default="examples.txt", help="The path of input example"
+    )
+    parser.add_argument("--log", default="log", help="The path of output log")
+    parser.add_argument(
+        "--tokenize_path",
+        "--t_path",
+        default=None,
+        help="The path of ChatGLM model",
+    )
+
+    parser.add_argument(
+        "--promote_path", "--p_path", default="promotes", help="The path of promotes"
+    )
 
     quantize_group = parser.add_mutually_exclusive_group()
-    quantize_group.add_argument('--low_vram', action='store_true', help='Use 4-bit quantization')
-    quantize_group.add_argument('--med_vram', action='store_true', help='Use 8-bit quantization')
+    quantize_group.add_argument(
+        "--low_vram", action="store_true", help="Use 4-bit quantization"
+    )
+    quantize_group.add_argument(
+        "--med_vram", action="store_true", help="Use 8-bit quantization"
+    )
 
-    parser.add_argument('--cpu', action='store_true', help='Use CPU')
+    parser.add_argument("--cpu", action="store_true", help="Use CPU")
 
-    parser.add_argument('--low_ram', action='store_true', help='Use CPU (low RAM)')
+    parser.add_argument("--low_ram", action="store_true", help="Use CPU (low RAM)")
 
     return parser.parse_args()
 
@@ -25,9 +46,24 @@ def get_args():
 args = get_args()
 
 if not os.path.isdir(args.path):
-    raise FileNotFoundError('Model not found')
+    raise FileNotFoundError("Model not found")
 
-tokenizer = AutoTokenizer.from_pretrained(args.path, trust_remote_code=True)
+if args.promote_path is None:
+    args.promote_path = args.path
+elif not os.path.isdir(args.promote_path):
+    raise FileNotFoundError("promotes not found")
+
+possible_promotes: list[str] = os.listdir(args.promote_path)
+possible_promotes.append("none")
+
+
+def update_possible_promotes():
+    possible_promotes = os.listdir(args.promote_path)
+    possible_promotes.append("none")
+    return gr.Dropdown.update(choices=possible_promotes)
+
+
+tokenizer = AutoTokenizer.from_pretrained(args.tokenize_path, trust_remote_code=True)
 model = AutoModel.from_pretrained(args.path, trust_remote_code=True)
 
 if args.cpu:
@@ -46,165 +82,236 @@ model = model.eval()
 
 
 def parse_text(text):
-    lines = text.split('\n')
-    for i, line in enumerate(lines):
-        if '```' in line:
-            item = line.split('`')[-1]
-            if item:
-                lines[i] = f'<pre><code class="{item}">'
-            else:
-                lines[i] = '</code></pre>'
-        else:
-            if i > 0:
-                line = line.replace('<', '&lt;').replace('>', '&gt;')
-                lines[i] = f'<br/>{line}'
-    return ''.join(lines)
+    # lines = text.split("\n")
+    # incode = False
+    # for i, line in enumerate(lines):
+    #     if "```" in line:
+    #         item = line.split("`")[-1]
+    #         if incode:
+    #             lines[i] = "</code></pre>"
+    #         elif item:
+    #             lines[i] = f'<pre><code class="{item}">'
+    #         else:
+    #             lines[i] = '<pre><code>'
+    #         incode = not incode
+    #     else:
+    #         if i > 0 and not incode:
+    #             line = line.replace("<", "&lt;").replace(">", "&gt;")
+    #             lines[i] = f"<br/>{line}"
+    # return "".join(lines)
+    return text
 
 
-def chat_wrapper(query, styled_history, history, max_length, top_p, temperature, memory_limit):
-    if query == '':
-        return [], [], ''
-    if memory_limit == 0:
-        history = []
-        styled_history = []
-    elif memory_limit > 0:
-        history = history[-memory_limit:]
-        styled_history = styled_history[-memory_limit:]
-    message, history = model.chat(tokenizer, query, history=history,
-                                  max_length=max_length, top_p=top_p, temperature=temperature)
-    styled_history.append((parse_text(query), parse_text(message)))
-    return styled_history, history, '', *gr_hide()
+def load_promote(file_name: str) -> tuple[str, str]:
+    with open(
+        file=os.path.join(args.promote_path, file_name), encoding="utf-8"
+    ) as promote_file:
+        dict_list = json.load(promote_file)
+        history = [
+            (restore_str(item["input"]), restore_str(item["output"]))
+            for item in dict_list
+        ]
+        return history[0]
 
 
-def regenerate_wrapper(styled_history, history, max_length, top_p, temperature, memory_limit):
-    if not history:
-        return [], [], '', *gr_hide()
+def chat_wrapper(
+    promote: str,
+    query: str,
+    styled_history: list[tuple[str, str]],
+    history: list[tuple[str, str]],
+    max_length: int,
+    top_p: float,
+    temperature: float,
+):
+    if promote != "none":
+        promot_val = load_promote(promote)
+        if len(history) == 0 and len(promot_val[1]) != 0:
+            history = [promot_val]
+        elif len(history) == 0 and len(promot_val[1]) == 0:
+            query = promot_val[0]
+        elif len(promot_val[1]) != 0:
+            history[0] = promot_val
+    message, history = model.chat(
+        tokenizer,
+        query,
+        history=history,
+        max_length=max_length,
+        top_p=top_p,
+        temperature=temperature,
+    )
+    styled_history = [(parse_text(h[0]), parse_text(h[1])) for h in history]
+    return styled_history, history, "", get_current_vram()
 
-    styled_history, history, query, _, _, _ = edit_wrapper(styled_history, history)
-    return chat_wrapper(query, styled_history, history, max_length, top_p, temperature, memory_limit)
+
+def regenerate_wrapper(
+    promote, query, styled_history, history, max_length, top_p, temperature
+):
+    if len(history) == 0:
+        return [], [], "", get_current_vram()
+    new_styled_history, new_history, query, vram = edit_wrapper(styled_history, history)
+    return chat_wrapper(
+        promote, query, new_styled_history, new_history, max_length, top_p, temperature
+    )
 
 
 def edit_wrapper(styled_history, history):
     if len(history) == 0:
-        return [], [], ''
+        return [], [], "", get_current_vram()
     query = history[-1][0]
     history = history[:-1]
     styled_history = styled_history[:-1]
-    return styled_history, history, query, *gr_hide()
+    return styled_history, history, query, get_current_vram()
 
 
 def reset_history():
-    return [], [], '', *gr_hide()
+    return [], [], "", get_current_vram()
 
 
-def save_history(history):
-    os.makedirs('log', exist_ok=True)
-    dict_list = [{'input': q, 'output': a} for q, a in history]
-    with open(f'log/{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.json', 'w', encoding='utf-8') as f:
+def cut_str(s: str):
+    return s.split("\n")
+
+
+def restore_str(s):
+    if isinstance(s, list):
+        return "\n".join(s)
+    return s
+
+
+def save_history(history, promote):
+    os.makedirs(args.log, exist_ok=True)
+    dict_list = [{"input": cut_str(q), "output": cut_str(a)} for q, a in history]
+    file_name = f'{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.json'
+    if promote != "none":
+        file_name = f"{os.path.splitext(promote)[0]}-{file_name}"
+    file_name = os.path.join(args.log, file_name)
+    with open(file_name, "w", encoding="utf-8") as f:
         json.dump(dict_list, f, ensure_ascii=False, indent=2)
+    return file_name
 
 
-def save_config(max_length=2048, top_p=0.7, temperature=0.95, memory_limit=-1.0):
-    with open('config.json', 'w') as f:
-        json.dump({'max_length': max_length, 'top_p': top_p, 'temperature': temperature, 'memory_limit': memory_limit}, f, indent=2)
+def save_config(max_length, top_p, temperature):
+    with open("config.json", "w") as f:
+        json.dump(
+            {"max_length": max_length, "top_p": top_p, "temperature": temperature},
+            f,
+            indent=2,
+        )
 
 
 def load_history(file, styled_history, history):
     current_styled_history, current_history = styled_history.copy(), history.copy()
     try:
-        with open(file.name, 'r', encoding='utf-8') as f:
+        with open(file.name, "r", encoding="utf-8") as f:
             dict_list = json.load(f)
-        history = [(item['input'], item['output']) for item in dict_list]
-        styled_history = [(parse_text(item['input']), parse_text(item['output'])) for item in dict_list]
+        history = [
+            (restore_str(item["input"]), restore_str(item["output"]))
+            for item in dict_list
+        ]
+        styled_history = [(parse_text(h[0]), parse_text(h[1])) for h in history]
     except BaseException:
-        return current_styled_history, current_history, ''
-    return styled_history, history, '', *gr_hide()
+        return current_styled_history, current_history, "", get_current_vram()
+    return styled_history, history, "", get_current_vram()
 
 
-def gr_show_and_load(history, evt: gr.SelectData):
-    if evt.index[1] == 0:
-        label = f'修改提问{evt.index[0]}：'
-    else:
-        label = f'修改回答{evt.index[0]}：'
-    return {'visible': True, '__type__': 'update'}, {'value': history[evt.index[0]][evt.index[1]], 'label': label, '__type__': 'update'}, evt.index
+def get_current_vram():
+    return torch.cuda.memory_allocated(0) / (1024**3)
 
 
-def update_history(styled_history, history, log, idx):
-    if log == '':
-        return styled_history, history, {'visible': True, '__type__': 'update'},  {'value': history[idx[0]][idx[1]], '__type__': 'update'}, idx
+def main():
+    with gr.Blocks(title="ChatGLM") as app:
+        if not os.path.isfile("config.json"):
+            save_config(4096, 0.7, 0.95)
 
-    def swap_value(lst, idx, value):
-        lst[idx[0]] = tuple(value if j == idx[1] else elem for j, elem in enumerate(lst[idx[0]]))
-        return lst
-    styled_history = swap_value(styled_history, idx, parse_text(log))
-    history = swap_value(history, idx, log)
-    return styled_history, history, *gr_hide()
+        with open("config.json", "r", encoding="utf-8") as f:
+            configs = json.loads(f.read())
+
+        gr.Markdown("""<h1><center>ChatGLM WebUI</center></h1>""")
+        with gr.Row():
+            with gr.Column():
+                with gr.Row():
+                    save = gr.Button("保存对话 （在 log 文件夹下）")
+                    file = gr.File(file_types=["json"])
+                    load = gr.UploadButton(
+                        "读取对话", file_types=["file"], file_count="single"
+                    )
+                promot = gr.Dropdown(possible_promotes, value="none")
+                gr.Button("加载promotes").click(update_possible_promotes, outputs=promot)
+
+                with gr.Accordion(label="设置"):
+                    gr.Markdown(
+                        """`Max Length` 是生成文本时的长度限制，`Top P` 控制输出文本中概率最高前 p 个单词的总概率，`Temperature` 控制生成文本的多样性和随机性。<br/>`Top P` 变小会生成更多样和不相关的文本；变大会生成更保守和相关的文本。<br/>`Temperature` 变小会生成更保守和相关的文本；变大会生成更奇特和不相关的文本。"""
+                    )
+                    with gr.Row():
+                        top_p = gr.Slider(
+                            minimum=0.01,
+                            maximum=1.0,
+                            step=0.01,
+                            label="Top P",
+                            value=configs["top_p"],
+                        )
+                        temperature = gr.Slider(
+                            minimum=0.01,
+                            maximum=1.0,
+                            step=0.01,
+                            label="Temperature",
+                            value=configs["temperature"],
+                        )
+                    max_length = gr.Slider(
+                        minimum=4.0,
+                        maximum=4096.0,
+                        step=4.0,
+                        label="Max Length",
+                        value=configs["max_length"],
+                    )
+                    save_conf = gr.Button("保存设置")
+            with gr.Column(scale=7):
+                gr.Markdown("""<h2>聊天记录</h2>""")
+                chatbot = gr.Chatbot(elem_id="chatbot", show_label=False)
+                vram = gr.Slider(
+                    maximum=torch.cuda.get_device_properties(0).total_memory
+                    / (1024**3),
+                    value=get_current_vram(),
+                    interactive=False,
+                    label="显存",
+                )
+                state = gr.State([])
+                message = gr.Textbox(placeholder="输入内容", label="用户：")
+                if os.path.isfile(args.example):
+                    with gr.Accordion(label="示例"):
+                        with open(args.example, encoding="utf-8") as e:
+                            gr.Examples(
+                                [i.strip() for i in e.readlines()],
+                                inputs=[message],
+                                examples_per_page=100,
+                            )
+                with gr.Row():
+                    submit = gr.Button("提交", variant="primary")
+                    edit = gr.Button("修改问题")
+                    regen = gr.Button("重新生成")
+                delete = gr.Button("清空聊天", variant="stop")
+
+        submit_list = [promot, message, chatbot, state, max_length, top_p, temperature]
+        state_list = [chatbot, state, message, vram]
+
+        save_conf.click(save_config, inputs=[max_length, top_p, temperature])
+        load.upload(load_history, inputs=[load, chatbot, state], outputs=state_list)
+        save.click(save_history, inputs=[state, promot], outputs=[file])
+        message.submit(
+            chat_wrapper,
+            inputs=submit_list,
+            outputs=state_list,
+        )
+        submit.click(
+            chat_wrapper,
+            inputs=submit_list,
+            outputs=state_list,
+            api_name="chat",
+        )
+        edit.click(edit_wrapper, inputs=submit_list[2:4], outputs=state_list)
+        regen.click(regenerate_wrapper, inputs=submit_list, outputs=state_list)
+        delete.click(reset_history, outputs=state_list)
+        app.launch(debug=True, server_name="0.0.0.0", share=True, show_api=False)
 
 
-def gr_hide():
-    return {'visible': False, '__type__': 'update'}, {'value': '', 'label': '', '__type__': 'update'}, []
-
-
-with gr.Blocks() as demo:
-    if not os.path.isfile('config.json'):
-        save_config()
-
-    with open('config.json', 'r', encoding='utf-8') as f:
-        configs = json.loads(f.read())
-
-    gr.Markdown('''<h1><center>ChatGLM WebUI</center></h1>''')
-    gr.Markdown('''`Max Length` 是生成文本时的长度限制，`Top P` 控制输出文本中概率最高前 p 个单词的总概率，`Temperature` 控制生成文本的多样性和随机性。<br/>`Top P` 变小会生成更多样和不相关的文本；变大会生成更保守和相关的文本。<br/>`Temperature` 变小会生成更保守和相关的文本；变大会生成更奇特和不相关的文本。<br/>`Memory Limit` 对话记忆轮数，`-1` 为无限长，限制记忆可减小显存占用。''')
-
-    with gr.Row():
-        max_length = gr.Slider(minimum=4.0, maximum=4096.0, step=4.0, label='Max Length', value=configs['max_length'])
-        top_p = gr.Slider(minimum=0.01, maximum=1.0, step=0.01, label='Top P', value=configs['top_p'])
-        temperature = gr.Slider(minimum=0.01, maximum=1.0, step=0.01, label='Temperature', value=configs['temperature'])
-        memory_limit = gr.Slider(minimum=-1.0, maximum=10.0, step=1.0, label='Memory Limit', value=configs['memory_limit'])
-    save_conf = gr.Button('保存设置')
-
-    gr.Markdown('''<h2>提示：点击对话可以进行修改</h2>''')
-
-    state = gr.State([])
-    chatbot = gr.Chatbot(elem_id='chatbot', show_label=False)
-    with gr.Row(visible=False) as edit_log:
-        with gr.Column():
-            log = gr.Textbox()
-            with gr.Row():
-                submit_log = gr.Button('保存')
-                cancel_log = gr.Button('取消')
-    log_idx = gr.State([])
-
-    message = gr.Textbox(placeholder='输入内容', label='问：')
-
-    with gr.Row():
-        submit = gr.Button('提交')
-        edit = gr.Button('修改上一问题')
-        regen = gr.Button('重新生成')
-
-    delete = gr.Button('清空聊天')
-
-    with gr.Row():
-        save = gr.Button('保存对话（在 `log` 文件夹下）')
-        load = gr.UploadButton('读取对话', file_types=['file'], file_count='single')
-
-    input_list = [message, chatbot, state, max_length, top_p, temperature, memory_limit]
-    output_list = [chatbot, state, message]
-    edit_list = [edit_log, log, log_idx]
-
-    save_conf.click(save_config, inputs=input_list[3:])
-    load.upload(load_history, inputs=[load, chatbot, state], outputs=output_list + edit_list)
-    save.click(save_history, inputs=[state])
-    message.submit(chat_wrapper, inputs=input_list, outputs=output_list + edit_list)
-    submit.click(chat_wrapper, inputs=input_list, outputs=output_list + edit_list)
-    edit.click(edit_wrapper, inputs=input_list[1:3], outputs=output_list + edit_list)
-    regen.click(regenerate_wrapper, inputs=input_list[1:], outputs=output_list + edit_list)
-    delete.click(reset_history, outputs=output_list + edit_list)
-    chatbot.select(gr_show_and_load, inputs=[state], outputs=edit_list)
-    edit_kwargs = {'inputs': [chatbot, state, log, log_idx], 'outputs': [chatbot, state] + edit_list} 
-    log.submit(update_history, **edit_kwargs)
-    submit_log.click(update_history, **edit_kwargs)
-    cancel_log.click(gr_hide, outputs=edit_list)
-
-
-if __name__ == '__main__':
-    demo.launch(debug=True)
+if __name__ == "__main__":
+    main()
